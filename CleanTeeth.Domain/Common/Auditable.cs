cbd35d377b59265df7bc5b7a8cac26c5119f49b1
GaoHan <869912332@qq.com>
2026-04-10 18:10:28 +0800

    feat

--- CleanTeeth.Domain/Common/Auditable.cs
+++ CleanTeeth.Domain/Common/Auditable.cs
@@ -3,7 +3,10 @@ namespace CleanTeeth.Domain.Common;
 public abstract class Auditable
 {
     public string? CreatedBy { get; set; }
+    // 这里使用 CreatedAt 更规范
     public DateTime? CreatedOn { get; set; }
+    // 这里使用 UpdatedBy 更规范
     public string? LastModifiedBy { get; set; }
+    // 这里使用 UpdatedAt 更规范
     public DateTime? LastModifiedOn { get; set; }
 }
